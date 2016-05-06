d6686350de95dc3fb9814dbac28d37c4fd3d3c93
Brian Jepson <bjepson@gmail.com>
2016-05-06 11:35:43 -0400

    switched to HSV for more control over detection

--- tests/find_shapes.py
+++ tests/find_shapes.py
@@ -5,20 +5,20 @@ from picamera import PiCamera
 import time
 import cv2
 
-def findColor(image, lower, upper):
+def findColor(img, lower, upper):
 
     found = False
-    shapeMask = cv2.inRange(image, lower, upper)
+    shapeMask = cv2.inRange(img, lower, upper)
 
     # find the contours in the mask
     (_, cnts, _) = cv2.findContours(shapeMask.copy(), cv2.RETR_EXTERNAL,
         cv2.CHAIN_APPROX_SIMPLE)
     print "I found %d shapes" % (len(cnts))
-#    cv2.imshow("Mask", shapeMask)
-#    while (1):
-#        if cv2.waitKey(0) == 27:
-#            break
-#    cv2.destroyAllWindows()
+    cv2.imshow("Mask", shapeMask)
+    while (1):
+        if cv2.waitKey(0) == 27:
+            break
+    cv2.destroyAllWindows()
 
     # loop over the contours
     for c in cnts:
@@ -27,7 +27,7 @@ def findColor(image, lower, upper):
         if area > 10000:
             print "book!"
             found = True
-            cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+            cv2.drawContours(img, [c], -1, (70, 255, 255), 2)
     return found
 
 # initialize the camera and grab a reference to the raw camera capture
@@ -40,17 +40,19 @@ time.sleep(0.1)
 # grab an image from the camera
 camera.capture(rawCapture, format="bgr")
 image = rawCapture.array
+hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
 # find all the 'green' shapes in the image
-lower = np.array([0, 60, 0])
-upper = np.array([30, 90, 30])
-green_found = findColor(image, lower, upper)
+lower = np.array([60, 70, 70])
+upper = np.array([110, 255, 255])
+green_found = findColor(hsv, lower, upper)
 
 # find all the 'violet' shapes in the image
-lower = np.array([45, 55, 85])
-upper = np.array([75, 85, 105])
-violet_found = findColor(image, lower, upper)
+lower = np.array([260/2, 70, 70])
+upper = np.array([280/2, 255, 255])
+violet_found = findColor(hsv, lower, upper)
 
+image = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
 cv2.imshow("Image", image)
 while (1):
     if cv2.waitKey(0) == 27:
