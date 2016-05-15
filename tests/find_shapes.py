f7f88d5652069030f5dba6d567f286b318e41d90
Brian Jepson <bjepson@gmail.com>
2016-05-15 10:34:48 -0400

    refinement to color calibration; started work on selection rect sizing

--- tests/find_shapes.py
+++ tests/find_shapes.py
@@ -10,15 +10,15 @@ import pprint
 import time
 
 hsv_ranges = {
-     'blue':      {'high': [105, 255, 255], 'low': [85, 50, 50]},
+     'blue':      {'high': [99, 255, 255], 'low': [85, 50, 50]},
      'fl yellow': {'high': [40, 175, 225], 'low': [30, 140, 190]},
-     'green':     {'high': [60, 255, 255], 'low': [40, 50, 50]},
-     'navy':      {'high': [110, 255, 145], 'low': [100, 60, 70]},
+     'green':     {'high': [75, 255, 255], 'low': [40, 50, 50]},
+     'navy':      {'high': [110, 255, 255], 'low': [100, 60, 70]},
      'orange':    {'high': [18, 255, 255], 'low': [5, 180, 200]},
-     'pink':      {'high': [180, 255, 255], 'low': [150, 100, 150]},
+     'pink':      {'high': [170, 255, 255], 'low': [150, 60, 120]},
      'red':       {'high': [5, 255, 255], 'low': [0, 100, 100]},
-     'violet':    {'high': [140, 150, 180], 'low': [130, 60, 75]},
-     'yellow':    {'high': [30, 255, 255], 'low': [20, 200, 200]}}
+     'violet':    {'high': [140, 150, 180], 'low': [110, 60, 75]},
+     'yellow':    {'high': [30, 255, 255], 'low': [20, 150, 150]}}
 
 shortcuts = {
         'g': 'green',
@@ -32,11 +32,27 @@ shortcuts = {
         'f': 'fl yellow',
         }
 
+rects = {
+        'green': (20, 500),
+        'blue': (20, 500),
+        'navy': (20, 500),
+        'pink': (20, 500),
+        'orange': (20, 500),
+        'red': (20, 500),
+        'violet': (20, 500),
+        'yellow': (20, 500),
+        'fl yellow': (20, 500),
+        }
+
 pp = pprint.PrettyPrinter()
 
+rect_x = 30
+rect_y = 333
 x_co = 0
 y_co = 0
 hsv_data = 0
+last_x_co = 0
+last_y_co = 0
 
 def on_mouse(event,x,y,flag,param):
   global x_co
@@ -64,17 +80,14 @@ def findColor(img, img2, lower, upper, color, screen, row):
     # loop over the contours
     for c in cnts:
         area = cv2.contourArea(c)
-        #print area
-        if area > 9000:
+        target_area = rects[color][0] * rects[color][1]
+        if area > target_area:
             status += " " + str(area)
             found = True
             cv2.drawContours(img2, [c], -1, (
                 (lower[0] * 1.0+ upper[0] * 1.0)/2, 
-                255, #(lower[1] + upper[1])/2, 
-                255 #(lower[2] + upper[2])/2, 
-                ), 4)
+                255, 255 ), 4)
     screen.addstr(row, 2, status)
-    #screen.addstr(row + 20, 2, status2)
     return found
 
 # initialize the camera and grab a reference to the raw camera capture
@@ -123,11 +136,19 @@ while (True):
     screen.refresh()
     pad.refresh(0,0, disp_row,1, disp_row+21,80)
 
-    hsv_data=hsv[y_co,x_co]
-    cv2.putText(hsv_copy, 
-                str(hsv_data[0])+","+str(hsv_data[1])+","+str(hsv_data[2]), 
-                (x_co,y_co),
-                cv2.FONT_HERSHEY_SIMPLEX, .5, (55,25,255), 2)
+    if y_co != last_y_co and x_co != last_x_co:
+        # overlay the HSV data of pixel under cursor
+        hsv_data=hsv[y_co,x_co]
+        cv2.putText(hsv_copy, 
+                    str(hsv_data[0])+","+str(hsv_data[1])+","+str(hsv_data[2]), 
+                    (x_co,y_co),
+                    cv2.FONT_HERSHEY_SIMPLEX, .5, (55,25,255), 2)
+
+
+        # draw the selection rectangle
+        hsv_copy = cv2.rectangle(hsv_copy, (x_co, y_co), (x_co+rect_x, y_co+rect_y), (179,255,255), 1)
+    last_y_co = y_co
+    last_x_co = x_co
 
     image = cv2.cvtColor(hsv_copy, cv2.COLOR_HSV2BGR)
     cv2.imshow("cv", image)
@@ -135,8 +156,12 @@ while (True):
     k = cv2.waitKey(10)
     if k == 27:
         break
-    if k == ord('a'):
+    if k == ord('c'):
         log.clear()
+    if k == ord('w'):
+        rect_y = rect_y + 10
+    if k == ord('s'):
+        rect_y = rect_y - 10
     for s,c in shortcuts.iteritems():
         if k == ord(s) and len(log) > 0:
             low  = []
@@ -146,14 +171,14 @@ while (True):
                     low = list(log[i])
                 else:
                     low[0] = min(low[0], log[i][0])
-                    low[1] = min(low[1], log[i][1])
-                    low[2] = min(low[2], log[i][2])
+                    low[1] = min(low[1], int(log[i][1] *.66))
+                    low[2] = min(low[2], int(log[i][2] *.66))
                 if len(high) == 0:
                     high = list(log[i])
                 else:
                     high[0] = max(high[0], log[i][0])
-                    high[1] = max(high[1], log[i][1])
-                    high[2] = max(high[2], log[i][2])
+                    high[1] = 255 #max(high[1], log[i][1])
+                    high[2] = 255 #max(high[2], log[i][2])
             hsv_ranges[c]['high'] = list(high)
             hsv_ranges[c]['low'] = list(low)
             log.clear()
